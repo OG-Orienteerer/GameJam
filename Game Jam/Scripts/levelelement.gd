5bde525f8e4dfb4f79386dfb1a76631613b93d38
John Lemos <152223636+OG-Orienteerer@users.noreply.github.com>
2024-07-28 20:03:00 -0400

    Delete Game Jam directory

--- Game Jam/Scripts/levelelement.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var target_lvl = ""
-
-func _on_button_pressed():
-	if Levelmanagement.levels[target_lvl]["unlocked"]:
-		Levelmanagement.load_lvl(target_lvl)
-	else:
-		print("Unlock this level by completing the previous levels first.")
